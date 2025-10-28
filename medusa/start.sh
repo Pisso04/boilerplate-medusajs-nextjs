62964751536f786424edbc6592a9d162f4a6b204
Pissano <itanagblonon4@gmail.com>
2025-10-28 10:15:38 +0100

    update start.sh scrip to add module drone_extension

--- medusa/start.sh
+++ medusa/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Add drone_extension module to Medusa
+echo "Adding drone_extension module to Medusa..."
+npx medusa db:generate drone_extension
+
 # Run migrations and start server
 echo "Running database migrations..."
 npx medusa db:migrate
